58d1b9b86323a90ebcad69e0b0345d5804064020
Mikołaj Kuranowski <mkuranowski@gmail.com>
2021-07-07 13:49:27 +0200

    Await for the parser to finish in the entry-point

--- polregiogtfs.sh
+++ polregiogtfs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 # Copyright (c) 2021 Mikołaj Kuranowski
 # SPDX-License-Identifier: MIT
-echo 'import { PolRegioGTFS } from "./src/parser.ts"; PolRegioGTFS.main();' |
+echo 'import { PolRegioGTFS } from "./src/parser.ts"; await PolRegioGTFS.main();' |
 deno run --unstable --allow-net --allow-read=gtfs --allow-write=gtfs --allow-run=zip -

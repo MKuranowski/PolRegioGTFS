7862726c00a9ca285767bb39698b215c027bc263
Mikołaj Kuranowski <mkuranowski@gmail.com>
2021-11-30 09:54:11 +0100

    Reformat files with new config

--- src/csv.ts
+++ src/csv.ts
@@ -11,48 +11,47 @@ type Stringable = string | null | undefined | { toString(): string };
 const needsEscape = /"|,|\r|\n/;
 
 /** Converts a `Stringable` object to a string */
-const stringify = (o: Stringable) =>
-  o === null || o === undefined ? "" : o.toString();
+const stringify = (o: Stringable) => o === null || o === undefined ? "" : o.toString();
 
 /** Escapes a string for exporting in CSV */
 const escapeCell = (cell: string) =>
-  needsEscape.test(cell) ? `"${cell.replaceAll('"', '""')}"` : cell;
+    needsEscape.test(cell) ? `"${cell.replaceAll('"', '""')}"` : cell;
 
 /** Serializes multiple objects to a CSV row, terminated with \r\n */
 export const toCSV = (row: Stringable[]) =>
-  row.map((e) => escapeCell(stringify(e))).join(",") + "\r\n";
+    row.map((e) => escapeCell(stringify(e))).join(",") + "\r\n";
 
 /**
  * CSVFile is a small abstraction over Deno.File to help with writing CSV files.
  */
 export class CSVFile {
-  handle: Deno.File;
-  private encoder = new TextEncoder();
+    handle: Deno.File;
+    private encoder = new TextEncoder();
 
-  /** Create a CSVFile on a particular file */
-  constructor(handle: Deno.File) {
-    this.handle = handle;
-  }
+    /** Create a CSVFile on a particular file */
+    constructor(handle: Deno.File) {
+        this.handle = handle;
+    }
 
-  /**
-   * Opens a CSV file for writing - creating a file if it does not exist,
-   * and truncating one if it were to exist.
-   * @param filename name of the file to open
-   */
-  static async open(filename: string | URL): Promise<CSVFile> {
-    return new CSVFile(
-      await Deno.open(filename, { write: true, truncate: true, create: true }),
-    );
-  }
+    /**
+     * Opens a CSV file for writing - creating a file if it does not exist,
+     * and truncating one if it were to exist.
+     * @param filename name of the file to open
+     */
+    static async open(filename: string | URL): Promise<CSVFile> {
+        return new CSVFile(
+            await Deno.open(filename, { write: true, truncate: true, create: true }),
+        );
+    }
 
-  /** Write a single row to the file */
-  async write_row(row: Stringable[]): Promise<void> {
-    const view = this.encoder.encode(toCSV(row));
-    await this.handle.write(view);
-  }
+    /** Write a single row to the file */
+    async write_row(row: Stringable[]): Promise<void> {
+        const view = this.encoder.encode(toCSV(row));
+        await this.handle.write(view);
+    }
 
-  /** Close the underlying file */
-  close() {
-    this.handle.close();
-  }
+    /** Close the underlying file */
+    close() {
+        this.handle.close();
+    }
 }
--- src/data.ts
+++ src/data.ts
@@ -6,15 +6,15 @@
 
 type Color = [string, string];
 type Route = {
-  code: string;
-  name: string;
-  color?: Color;
-  busColor?: Color;
+    code: string;
+    name: string;
+    color?: Color;
+    busColor?: Color;
 };
 type Agency = {
-  name: string;
-  url: string;
-  phone?: string;
+    name: string;
+    url: string;
+    phone?: string;
 };
 
 export const DEFAULT_COLOR: Color = ["E50000", "FFFFFF"];
@@ -23,20 +23,20 @@ export const BUS_CODE_PREFIX = "ZKA ";
 export const BUS_NAME_SUFFIX = " - Zastępcza Komunikacja Autobusowa";
 
 export const ATTRS = {
-  BIKES: 9,
-  WHEELCHAIR: 10,
-  BIKES_ALLOWED: 50,
-  BUS: 56,
-  REPLACEMENT_BUS: 100,
+    BIKES: 9,
+    WHEELCHAIR: 10,
+    BIKES_ALLOWED: 50,
+    BUS: 56,
+    REPLACEMENT_BUS: 100,
 };
 
 export const ROUTES: Map<number, Route> = new Map([
-  [3, { code: "REG", name: "Regio" }],
-  [4, { code: "IR", name: "interRegio" }],
-  [20, { code: "PRS", name: "PolRegio - Specjalny" }],
-  [48, { code: "SR", name: "superRegio" }],
+    [3, { code: "REG", name: "Regio" }],
+    [4, { code: "IR", name: "interRegio" }],
+    [20, { code: "PRS", name: "PolRegio - Specjalny" }],
+    [48, { code: "SR", name: "superRegio" }],
 ]);
 
 export const AGENCIES: Map<number, Agency> = new Map([
-  [4, { name: "PolRegio", url: "https://polregio.pl/", phone: "+48703202020" }],
+    [4, { name: "PolRegio", url: "https://polregio.pl/", phone: "+48703202020" }],
 ]);

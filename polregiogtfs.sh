ed2e66df8e78d1ae920425852c2482a8e3e06fe2
Mikołaj Kuranowski <mkuranowski@gmail.com>
2024-06-25 08:31:33 +0200

    Remove --unstable deno flag

--- polregiogtfs.sh
+++ polregiogtfs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-# Copyright (c) 2021 Mikołaj Kuranowski
+# Copyright (c) 2021, 2024 Mikołaj Kuranowski
 # SPDX-License-Identifier: MIT
 echo 'import { PolRegioGTFS } from "./src/parser.ts"; await PolRegioGTFS.main();' |
-deno run --unstable --allow-net --allow-read=gtfs --allow-write=gtfs --allow-run=zip -
+deno run --allow-net --allow-read=gtfs --allow-write=gtfs --allow-run=zip -

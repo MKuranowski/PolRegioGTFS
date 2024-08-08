c28ca3565a681608dcd5b280eac05bb16266ce8f
Mikołaj Kuranowski <mkuranowski@gmail.com>
2024-08-08 21:14:34 +0200

    Remove TS implementation

--- polregiogtfs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-# Copyright (c) 2021, 2024 Mikołaj Kuranowski
-# SPDX-License-Identifier: MIT
-echo 'import { PolRegioGTFS } from "./src/parser.ts"; await PolRegioGTFS.main();' |
-deno run --allow-net --allow-read=gtfs --allow-write=gtfs --allow-run=zip -

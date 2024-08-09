055a5e2ceb1a3942cc36bb47d5ef6710c5032adf
Mikołaj Kuranowski <mkuranowski@gmail.com>
2024-08-09 14:40:02 +0200

    Normalize platform numbers

--- polregio_gtfs/cli.py
+++ polregio_gtfs/cli.py
@@ -25,6 +25,30 @@ class PolRegioGTFS(impuls.App):
                 ),
                 ScrapeAPI(),
                 LoadStationData(),
+                impuls.tasks.ExecuteSQL(
+                    task_name="NormalizePlatforms",
+                    statement=(
+                        "UPDATE stop_times SET platform = CASE "
+                        "  WHEN platform = 'I' THEN '1' "
+                        "  WHEN platform = 'Ia' THEN '1a' "
+                        "  WHEN platform = 'Ib' THEN '1b' "
+                        "  WHEN platform = 'II' THEN '2' "
+                        "  WHEN platform = 'IIa' THEN '2a' "
+                        "  WHEN platform = 'III' THEN '3' "
+                        "  WHEN platform = 'IIIa' THEN '3a' "
+                        "  WHEN platform = 'IV' THEN '4' "
+                        "  WHEN platform = 'IVa' THEN '4a' "
+                        "  WHEN platform = 'V' THEN '5' "
+                        "  WHEN platform = 'VI' THEN '6' "
+                        "  WHEN platform = 'VII' THEN '7' "
+                        "  WHEN platform = 'VIII' THEN '8' "
+                        "  WHEN platform = 'IX' THEN '9' "
+                        "  WHEN platform = 'X' THEN '10' "
+                        "  WHEN platform = 'XI' THEN '11' "
+                        "  ELSE platform "
+                        "END"
+                    ),
+                ),
                 impuls.tasks.GenerateTripHeadsign(),
                 impuls.tasks.SaveGTFS(
                     headers={
@@ -57,6 +81,7 @@ class PolRegioGTFS(impuls.App):
                             "stop_id",
                             "arrival_time",
                             "departure_time",
+                            "platform",
                         ),
                         "calendar_dates": ("service_id", "date", "exception_type"),
                     },
